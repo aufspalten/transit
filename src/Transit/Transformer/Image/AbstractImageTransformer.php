fa4035d15ac5a62f8dc0e827de2c6072a4a14c56
Daniele Colangelo <daniele.colangelo@walls.io>
2022-09-28 15:20:03 +0200

    Add webp support

--- src/Transit/Transformer/Image/AbstractImageTransformer.php
+++ src/Transit/Transformer/Image/AbstractImageTransformer.php
@@ -60,6 +60,9 @@ abstract class AbstractImageTransformer extends AbstractTransformer {
             case 'image/pjpeg':
                 $sourceImage = @imagecreatefromjpeg($sourcePath);
             break;
+            case 'image/webp':
+                $sourceImage = @imagecreatefromwebp($sourcePath);
+            break;
             default:
                 $sourceImage = false;
             break;
@@ -144,6 +147,9 @@ abstract class AbstractImageTransformer extends AbstractTransformer {
             case 'image/jpeg':
             case 'image/pjpeg':
                 imagejpeg($targetImage, $targetPath, $options['quality']);
+            break;
+            case 'image/webp':
+                imagewebp($targetImage, $targetPath, $options['quality']);
             break;
         }
 
